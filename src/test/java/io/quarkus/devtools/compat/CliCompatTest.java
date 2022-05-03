e2c7a088fd6665dd5bfef42f763281e0215d35df
Andy Damevin <ia3andy@gmail.com>
2022-05-03 11:48:39 +0200

    Fix maven local repo for CLI runner and mvn clean intall

--- src/test/java/io/quarkus/devtools/compat/CliCompatTest.java
+++ src/test/java/io/quarkus/devtools/compat/CliCompatTest.java
@@ -19,7 +19,6 @@ import org.zeroturnaround.exec.stream.LogOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -43,9 +42,11 @@ public class CliCompatTest {
     private static final Path STORAGE_FILE = Path.of("./storage/cli-compat-test.json");
     private static final String ECOSYSTEM_CI = "ECOSYSTEM_CI";
     private static final String SNAPSHOT_VERSION = "999-SNAPSHOT";
+    private static final String MAVEN_CENTRAL_QUARKUS_REPO = "https://repo1.maven.org/maven2/io/quarkus/";
     private static WebClient client = WebClient.create(Vertx.vertx());
     private static Storage storage;
     private static Set<Combination> tested = new HashSet<>();
+    private static volatile boolean quarkusRepoTrusted = false;
 
     @BeforeAll
     public static void beforeAll() throws IOException {
@@ -60,8 +61,9 @@ public class CliCompatTest {
             store();
         }
     }
+
     public static void store() throws IOException {
-        if(isEcosystemCI()) {
+        if (isEcosystemCI()) {
             return;
         }
         Files.createDirectories(Path.of("./storage"));
@@ -126,42 +128,39 @@ public class CliCompatTest {
     }
 
     private DynamicTest testCombination(Path tempDir, Combination c) {
-       return DynamicTest.dynamicTest("Test CLI " + c.cli + " with Platform " + c.platform, () -> {
-           if (storage.ignored.contains(c)) {
-               System.out.println("This combination is set to be ignored: " + c);
-               return;
-           }
-           if (storage.verified.contains(c)) {
-               System.out.println("This combination has already been verified: " + c);
-               return;
+        return DynamicTest.dynamicTest("Test CLI " + c.cli + " with Platform " + c.platform, () -> {
+            if (storage.ignored.contains(c)) {
+                System.out.println("This combination is set to be ignored: " + c);
+                return;
             }
-           tested.add(c);
-           testCLI(tempDir.resolve("cli_" + c.cli + "-platform_" + c.platform), c);
-           storage.verified.add(c);
-           store();
-       });
+            if (storage.verified.contains(c)) {
+                System.out.println("This combination has already been verified: " + c);
+                return;
+            }
+            tested.add(c);
+            testCLI(tempDir.resolve("cli_" + c.cli + "-platform_" + c.platform), c);
+            storage.verified.add(c);
+            store();
+        });
 
     }
 
     public void testCLI(Path tempDir, Combination combination) throws IOException, InterruptedException, TimeoutException {
-
         tempDir.toFile().mkdirs();
-
-        String trust = jbang(tempDir, "trust", "add", "https://repo1.maven.org/maven2/io/quarkus/");
-
-        assertThat(trust, matchesPattern("(?s).*Adding .https://repo1.maven.org/maven2/io/quarkus/. to .*/trusted-sources.json.*"));
-
-        String appname = "qs-" + combination.cli.replace(".","_");
-        String output = jbang(tempDir, "alias", "add", "-f", ".", "--name="+appname, "https://repo1.maven.org/maven2/io/quarkus/quarkus-cli/" + combination.cli + "/quarkus-cli-"+combination.cli +"-runner.jar");
+        trustQuarkusRepo(tempDir);
+        String appName = "qs-" + combination.cli.replace(".", "_");
+        String repoDir = Objects.equals(combination.cli, SNAPSHOT_VERSION) ? getQuarkusMavenRepoLocal() : MAVEN_CENTRAL_QUARKUS_REPO;
+        String output = jbang(tempDir, "alias", "add", "-f", ".", "--name=" + appName, repoDir + "quarkus-cli/" + combination.cli + "/quarkus-cli-" + combination.cli + "-runner.jar");
 
         assertThat(output, matchesPattern(".jbang. Alias .* added .*\n"));
-        List<String> commands = List.of(appname, "create", "-P", "io.quarkus.platform::" + combination.platform, "demoapp");
+        List<String> commands = List.of(appName, "create", "-P", "io.quarkus.platform::" + combination.platform, "demoapp");
         propagateSystemPropertyIfSet("maven.repo.local", commands);
         String createResult = jbang(tempDir, commands);
 
         assertThat(tempDir.toFile(), aFileWithSize(greaterThan(1L)));
-
-        int result = run(tempDir.resolve("demoapp"), "mvn", "clean", "package")
+        List<String> mvnCommands = List.of("mvn", "clean", "package");
+        propagateSystemPropertyIfSet("maven.repo.local", mvnCommands);
+        int result = run(tempDir.resolve("demoapp"), mvnCommands)
             .redirectOutputAlsoTo(new LogOutputStream() {
                 @Override
                 protected void processLine(String s) {
@@ -171,20 +170,33 @@ public class CliCompatTest {
 
         assertThat(result, equalTo(0));
     }
-    String jbang(Path workingDir, List<String> args) throws IOException, InterruptedException, TimeoutException {
+
+    static String getQuarkusMavenRepoLocal() {
+        return System.getProperty("maven.repo.local", System.getProperty("user.home", "~") + "/.m2/repository") + "/io/quarkus/";
+    }
+
+    static void trustQuarkusRepo(Path tempDir) throws IOException, InterruptedException, TimeoutException {
+        if (!quarkusRepoTrusted) {
+            String trust = jbang(tempDir, "trust", "add", MAVEN_CENTRAL_QUARKUS_REPO);
+            assertThat(trust, matchesPattern("(?s).*Adding ." + MAVEN_CENTRAL_QUARKUS_REPO + ". to .*/trusted-sources.json.*"));
+        }
+    }
+
+    static String jbang(Path workingDir, List<String> args) throws IOException, InterruptedException, TimeoutException {
         List<String> realArgs = new ArrayList<>();
         realArgs.add(Path.of("./jbang").toAbsolutePath().toString());
         realArgs.addAll(args);
 
-        return run(workingDir, realArgs.toArray(new String[0])).execute().outputUTF8();
-    }
-    String jbang(Path workingDir, String... args) throws IOException, InterruptedException, TimeoutException {
-       return jbang(workingDir, Arrays.asList(args));
+        return run(workingDir, realArgs).execute().outputUTF8();
     }
 
-    ProcessExecutor run(Path workingDir, String... args) throws IOException, InterruptedException, TimeoutException {
+    static String jbang(Path workingDir, String... args) throws IOException, InterruptedException, TimeoutException {
+        return jbang(workingDir, Arrays.asList(args));
+    }
+
+    static ProcessExecutor run(Path workingDir, List<String> args) throws IOException, InterruptedException, TimeoutException {
         List<String> realArgs = new ArrayList<>();
-        realArgs.addAll(Arrays.asList(args));
+        realArgs.addAll(args);
 
         System.out.println("run: " + String.join(" ", realArgs));
         return new ProcessExecutor().command(realArgs)
